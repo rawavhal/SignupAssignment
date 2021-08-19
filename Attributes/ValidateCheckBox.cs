aa66de622a622b89d1b63c55ca2f0cbe83d36049
rawavhal <89210176+rawavhal@users.noreply.github.com>
2021-08-19 21:13:46 +0530

    Add files via upload

---
+++ Attributes/ValidateCheckBox.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Signup.Attributes
+{
+    public class ValidateCheckBox : ValidationAttribute, IClientModelValidator
+    {
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            context.Attributes.Add("data-val-checkbox", ErrorMessage);
+        }
+        public override bool IsValid(object value)
+        {
+            return (bool)value;
+        }
+    }
+}
