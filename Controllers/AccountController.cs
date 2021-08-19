d64eb03cd5ad7a03eeabdf403788a2af631509ea
rawavhal <89210176+rawavhal@users.noreply.github.com>
2021-08-19 21:20:17 +0530

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Signup.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Signup.Controllers
+{
+    public class AccountController : Controller
+    {
+        List<Country> CountryList;
+
+        List<City> CityList;       
+
+        public AccountController()
+        {
+
+            CountryList = new List<Country>()
+            {
+                new Country(){CountryId=1, CountryName="India"},
+                new Country (){ CountryId=2, CountryName="Japan"}
+            };
+
+            CityList = new List<City>()
+            {
+                new City(){CityId=1, CityName="Pune", CountryId=1},
+                new City(){CityId=2, CityName="Mumbai", CountryId=1},
+                new City(){CityId=3, CityName="Tokyo", CountryId=2},
+                new City(){CityId=4, CityName="Yokohama", CountryId=2}
+            };
+          
+        }
+
+
+        public JsonResult GetCity(int CountryId)
+        {
+
+            List<City> Cities = CityList.FindAll(x => x.CountryId == CountryId);
+            return new JsonResult(Cities);
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Signup()
+        {
+            ViewBag.CountryList = new SelectList(CountryList, "CountryId", "CountryName");          
+            
+            ViewData["Gender"] = new List<string>()
+            {
+                new string("Male"),
+                new string("Female"),
+                new string("Transgender")
+            }; 
+           
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Signup(UserModel usermodel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("DisplayMessage", "Account");
+            }
+            return View();
+        }
+
+        public IActionResult DisplayMessage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Signup.Attributes;
+
+namespace Signup.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Please Enter Name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Username.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name="Confirm Password")]
+        [Required(ErrorMessage = "Please Enter Confirm Password.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password should match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage ="Please enter Contact Number.")]
+        [RegularExpression("[6,7,8,9][0-9]{9}")]
+        [StringLength(10, MinimumLength =10,ErrorMessage ="Please enter 10 digits.")]
+        public string Contact { get; set; }
+
+        [Required(ErrorMessage ="Please select Country.")]
+        public string Country { get; set; }
+        public List<Country> CountryList { get; set; }
+
+        [Required(ErrorMessage = "Please select City.")]
+        public string City { get; set; }
+        public List<City> CityList { get; set; }
+
+        [Required(ErrorMessage = "Please select Gender.")]        
+        public string Gender { get; set; }      
+
+        [ValidateCheckBox(ErrorMessage ="Please Accept Terms.")]
+        [Display(Name ="Accept Terms")]
+        public bool Terms { get; set; }
+
+    }
+
+    public class Country
+    {
+        public int CountryId { get; set; }
+
+        public string CountryName { get; set; }
+    }
+
+    public class City
+    {
+        public int CountryId { get; set; }
+
+        public int CityId { get; set; }
+
+        public string CityName { get; set; }
+    }
+   
+}
